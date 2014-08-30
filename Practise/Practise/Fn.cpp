be6eebdf00d76a015095dfff5c5f602723db7a51
wangyuefive <wangyuefive@163.com>
2014-08-30 21:19:46 +0800

    更新注释

--- Practise/Practise/Fn.cpp
+++ Practise/Practise/Fn.cpp
@@ -8,8 +8,10 @@
 
 /**
 * 获取 1 出现的次数
+* 这是老办法，通过取余数和除法得出一个数的中1的个数
+* 这个算法有个缺陷，除法和取余数太耗费时间
 */
-int getOneCount(int num)
+int getOneCount(unsigned int num)
 {
 	int count = 0;
 	while(num > 0)
@@ -21,20 +23,48 @@ int getOneCount(int num)
 	return count ;
 }
 
+
+/**
+* 输入：112334222  ---->1221324123   数字+个数 的显示方式
+*/
+void printfStr(char *s)
+{
+	int count = 1 ;
+	int i =0 ;
+	printf("%s\n",s);
+	for(i = 0; *(s+i) !='\0'; i++)
+	{
+		if(*(s+i) == *(s+i+1))
+			count++;
+		else{
+			printf("%c%c",*(s+i),count+'0');
+			count=1;
+		}
+	}
+}
+
 int main(void)
 {
-	double MAX = 4000000000;   //刚开始定义为了 int 型，结果直接超了
-	int i = 0;
+	unsigned int MAX = 4000000000;   //刚开始定义为了 int 型，结果直接超了
+	unsigned int i = 0;
 	int count = 0 ;
+	int resultcount = 0;
 
-	printf("%d\n",getOneCount(101));
+	printfStr("12333422222");
+	timeb t1,t2;
+	ftime(&t1);
+	
 	for(i = 0 ; i < MAX ; i++)
 	{
 		count = count + getOneCount(i);
-		if(i == count)
+		if(i == count){
+  			resultcount++;
 			printf("---------%d\n",i);
+		}
 	}
-	printf("------------------end------------------");
+	ftime(&t2);
+	printf("------------------end---------------\n");
+	printf("-----time used:%dminute%dsec , sum: %d",t2.time-t1.time,t2.millitm-t1.millitm,resultcount);
 	getchar();
 	return 0;
 }
